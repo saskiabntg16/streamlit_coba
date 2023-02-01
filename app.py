e1f201cf7725a9b9f60f4c55ad2713ef9715164e
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-01 17:06:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,3 @@ if app_mode=='Home':
     st.markdown('Dataset :') 
     df=pd.read_csv('iris.csv') #Read our data dataset
     st.write(df.head()) 
-
-
-if app_mode=='Home': ## if someone selects the Home Tab
-st.title('Iris Classification Prediction') ##Display text in title formatting
-st.markdown('Dataset :') ## Display string formatted as Markdown.
-st.write(df.head()) #write and display out dataset using the command df.head
