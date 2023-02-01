d50b8b657564dadbdb225aa6e35d41721ba2f2e8
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-01 20:56:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ if app_mode=='Home':
     st.write(df.head()) 
 
     if app_mode=='Home': ## if someone selects the Home Tab
-st.title('Iris Classification Prediction') ##Display text in title formatting
 st.markdown('Dataset :') ## Display string formatted as Markdown.
 st.write(df.head()) #write and display out dataset using the command df.head
 
